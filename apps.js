95590ec6092856fd9848d90f4979b175c2bff8c0
tormod17 <tormodsmith@gmail.com>
2015-04-07 22:10:14 +0100

    updated with jquery

--- apps.js
+++ apps.js
@@ -3,6 +3,11 @@
 /*var fizz =	i % 3; 
 var buzz = x/5;
 var fizzBuzz= x/3 && x/5;*/
+
+
+
+var i=0;
+
 function fizzbuzz() { 
 
 	for (i=1; i<=100; i ++) {
@@ -12,13 +17,24 @@ function fizzbuzz() {
 
 
 			console.log('fizz buzz');
-			document.write('fizz buzz ,');
-		
+			document.write('fizz buzz');
+			
+			$(document).ready(function() {
+			$('.drawingBoard').append('<li>' + 'fizz buzz' + '</li>')
+			
+
+			
 		} else if ( i% 3 == 0 ) {
 
 			
 			console.log('fizz') ;
-			document.write('fizz ,');
+			document.write('fizz ,')
+
+			$(document).ready(function() {
+			$('.drawingBoard').append('<li>' + 'fizz' +'</li>')
+			});
+
+
 
 
 		
@@ -26,16 +42,32 @@ function fizzbuzz() {
 
 			
 			console.log('buzz');
-			document.write ('buzz ,');
-		
+			document.write('buzz ,')
+			$(document).ready(function() {
+			$('.drawingBoard').append('<li>' + 'buzz' + '</li>')
+			});
+
 		} else {
 
+			var counter= i;
 			console.log(i);
-			document.write(i + ',');
+			document.write( i + ', ');
+
+			$(document).ready(function() {
+			$('.drawingBoard').append('<li>' + counter + '</li>' )
+			});
+
+
+			
 		}
+		
+
 		
 	};
 };
 
 fizzbuzz();
+
+
+
 
